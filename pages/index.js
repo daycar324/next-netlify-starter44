d0fcd1aafb5d096116545693297f6e373779c406
daycar324 <160769121+daycar324@users.noreply.github.com>
2024-02-21 14:48:20 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
 <head>
-<base target="_self">
+<base target="_self"/>
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"/>
